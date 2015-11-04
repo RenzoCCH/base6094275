928c9d27ab412a545f11d7417827ff9e6df82754
RenzoCCH <renzocallachavez@gmail.com>
2015-11-03 21:08:32 -0400

    adding the modernizr

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -1,12 +1,14 @@
 <!DOCTYPE html>
-<html>
+<html class="no-js">
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta http-equiv="X-UA-Compatible" content="IE=edge,IE=10,IE=EmulateIE10,IE=9,IE=EmulateIE9,IE=8,IE=EmulateIE8,IE=7,IE=EmulateIE7" />
 
     <title>Base</title>
-    {!! Html::style('assets/css/styles.css') !!}
+    {!! Html::style('assets/css/styles.css') !!
+
+    {!! Html::js('assets/js/')!!}
 </head>
 <body>
 
