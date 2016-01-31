be46a31069fc966c7fe8a4236835cb840d5dd5c9
RenzoCCH <renzocallachavez@gmail.com>
2016-01-30 22:50:54 -0400

    adding partial enhanced view

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -1,14 +1,14 @@
 <!DOCTYPE html>
-<html class="no-js">
+<html class="no-js" ng-app="app">
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta http-equiv="X-UA-Compatible" content="IE=edge,IE=10,IE=EmulateIE10,IE=9,IE=EmulateIE9,IE=8,IE=EmulateIE8,IE=7,IE=EmulateIE7" />
 
     <title>Base</title>
-    {!! Html::style('assets/css/styles.css') !!
+    {!! Html::style('assets/css/styles.css') !!}
 
-    {!! Html::js('assets/js/')!!}
+    {!! Html::script('assets/js/modernizr-custom.js')!!}
 </head>
 <body>
 
@@ -18,5 +18,9 @@
       @endcan
       @yield('content')
     </main>
+
+     {!! Html::script('assets/js/jquery-1.11.3.min.js')!!}
+     {!! Html::script('assets/lib/angular.min.js')!!}
+     {!! Html::script('assets/app/app.js')!!}
 </body>
 </html>
