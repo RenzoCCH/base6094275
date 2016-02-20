5204bb07bf032fc85e325f392509056e393600c0
RenzoCCH <renzocallachavez@gmail.com>
2016-02-20 12:21:55 -0400

    adding all gulp implementation

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,9 @@ var gulp  = require('gulp');
 var compass = require('gulp-compass');
 var cssnano = require('gulp-cssnano');
 var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+var browserSync = require('browser-sync').create();
+var open = require('gulp-open');
 
 
 
@@ -16,14 +19,64 @@ gulp.task('compass', function() {
             cache: false
         }))
         .pipe(cssnano())
-        .pipe(gulp.dest('public/assets/css'));
-});
-gulp.task('scripts', function() {
-    return gulp.src('./lib/*.js')
-        .pipe(concat('all.js'))
-        .pipe(gulp.dest('./dist/'));
+        .pipe(gulp.dest('public/assets/css'))
+        .pipe(browserSync.reload({
+            stream: true
+        }));
 });
 
 
+//--------------for scripts-------------
+gulp.task('scripts', ['headscripts','footerscripts']);
 
-gulp.task('default',['compass']);
+gulp.task('bower-concat', function() {
+    return gulp.src(['public/bower_components/jquery/dist/jquery.min.js', 'public/angular/angular.min.js'])
+        .pipe(concat('bower.min.js'))
+        .pipe(gulp.dest('public/assets/dist/'));
+});
+
+
+
+gulp.task('headscripts', function() {
+    return gulp.src(['public/assets/js/modernizr-custom.js', 'public/bower_components/webcomponentsjs/webcomponents.min.js'])
+        .pipe(concat('header.min.js'))
+        .pipe(gulp.dest('public/assets/dist/'));
+});
+gulp.task('footerscripts',['uglifyapp'], function() {
+    return gulp.src(['public/assets/dist/bower.min.js', 'public/assets/dist/uglifyapp.min.js'])
+        .pipe(concat('all.min.js'))
+        .pipe(gulp.dest('public/assets/dist/'));
+});
+gulp.task('uglifyapp', function() {
+    return gulp.src( 'resources/assets/app/**/*.js')
+        .pipe(concat('uglifyapp.min.js'))
+        .pipe(uglify())
+        .pipe(gulp.dest('public/assets/dist/'));
+});
+//--------------------------------------
+
+gulp.task('watch', function(){
+    gulp.watch('resources/assets/sass/**/*.scss', ['compass']);
+    gulp.watch('resources/views/**/*.php',browserSync.reload);
+    // Other watchers
+})
+gulp.task('browserSync', function() {
+    browserSync.init({
+        proxy: {
+            target: "base"
+        }
+    })
+})
+gulp.task('app', function(){
+    var options = {
+        uri: 'base',
+        app: 'chrome'
+    };
+    gulp.src(__filename)
+        .pipe(open(options));
+});
+
+
+
+
+gulp.task('default',['app','browserSync','compass','scripts','watch']);
--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -7,8 +7,11 @@
 
     <title>Base</title>
     {!! Html::style('assets/css/main.css') !!}
-    {!! Html::script('assets/js/modernizr-custom.js')!!}
-    {!! Html::script('bower_components/webcomponentsjs/webcomponents.js')!!}
+
+    {{--{!! Html::script('assets/js/modernizr-custom.js')!!}--}}
+    {{--{!! Html::script('bower_components/webcomponentsjs/webcomponents.js')!!}--}}
+    {!! Html::script('assets/dist/header.min.js')!!}
+
     {{--adding polymeer dom--}}
     <link rel="import" href="{{url()}}/elements/button-loading.html">
 </head>
@@ -21,9 +24,10 @@
       @yield('content')
     </main>
 
-     {!! Html::script('bower_components/jquery/dist/jquery.min.js')!!}
-     {!! Html::script('bower_components/angular/angular.js')!!}
+     {{--{!! Html::script('bower_components/jquery/dist/jquery.min.js')!!}--}}
+     {{--{!! Html::script('bower_components/angular/angular.js')!!}--}}
+     {{--{!! Html::script('assets/app/app.js')!!}--}}
 
-     {!! Html::script('assets/app/app.js')!!}
+	{!! Html::script('assets/dist/all.min.js')!!}
 </body>
 </html>
