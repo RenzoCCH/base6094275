43ce3daddceb84ca2a18bde36ca63e06b9c7ff82
RenzoCCH <renzocallachavez@gmail.com>
2016-08-10 21:08:53 -0400

    adding celphone implementation for grid

--- app/Http/Helpers/Helpers.php
+++ app/Http/Helpers/Helpers.php
@@ -5,50 +5,51 @@
 
 function getBrowserLocale()
 {
-    // Credit: https://gist.github.com/Xeoncross/dc2ebf017676ae946082
-    $websiteLanguages = ['EN', 'ES'];
-    // Parse the Accept-Language according to:
-    // http://www.w3.org/Protocols/rfc2616/rfc2616-sec14.html#sec14.4
-    preg_match_all(
-        '/([a-z]{1,8})' .       // M1 - First part of language e.g en
-        '(-[a-z]{1,8})*\s*' .   // M2 -other parts of language e.g -us
-        // Optional quality factor M3 ;q=, M4 - Quality Factor
-        '(;\s*q\s*=\s*((1(\.0{0,3}))|(0(\.[0-9]{0,3}))))?/i',
-        $_SERVER['HTTP_ACCEPT_LANGUAGE'],
-        $langParse);
+	if(isset($_SERVER['HTTP_ACCEPT_LANGUAGE'])) {
+		// Credit: https://gist.github.com/Xeoncross/dc2ebf017676ae946082
+		$websiteLanguages = ['EN', 'ES'];
+		// Parse the Accept-Language according to:
+		// http://www.w3.org/Protocols/rfc2616/rfc2616-sec14.html#sec14.4
 
-    $langs = $langParse[1]; // M1 - First part of language
-    $quals = $langParse[4]; // M4 - Quality Factor
+		preg_match_all(
+			'/([a-z]{1,8})' .       // M1 - First part of language e.g en
+			'(-[a-z]{1,8})*\s*' .   // M2 -other parts of language e.g -us
+			// Optional quality factor M3 ;q=, M4 - Quality Factor
+			'(;\s*q\s*=\s*((1(\.0{0,3}))|(0(\.[0-9]{0,3}))))?/i',
+			$_SERVER['HTTP_ACCEPT_LANGUAGE'],
+			$langParse);
 
-    $numLanguages = count($langs);
-    $langArr = array();
 
-    for ($num = 0; $num < $numLanguages; $num++)
-    {
-        $newLang = strtoupper($langs[$num]);
-        $newQual = isset($quals[$num]) ?
-            (empty($quals[$num]) ? 1.0 : floatval($quals[$num])) : 0.0;
+		$langs = $langParse[1]; // M1 - First part of language
+		$quals = $langParse[4]; // M4 - Quality Factor
 
-        // Choose whether to upgrade or set the quality factor for the
-        // primary language.
-        $langArr[$newLang] = (isset($langArr[$newLang])) ?
-            max($langArr[$newLang], $newQual) : $newQual;
-    }
+		$numLanguages = count($langs);
+		$langArr = array();
 
-    // sort list based on value
-    // langArr will now be an array like: array('EN' => 1, 'ES' => 0.5)
-    arsort($langArr, SORT_NUMERIC);
+		for ($num = 0; $num < $numLanguages; $num++) {
+			$newLang = strtoupper($langs[$num]);
+			$newQual = isset($quals[$num]) ?
+				(empty($quals[$num]) ? 1.0 : floatval($quals[$num])) : 0.0;
 
-    // The languages the client accepts in order of preference.
-    $acceptedLanguages = array_keys($langArr);
+			// Choose whether to upgrade or set the quality factor for the
+			// primary language.
+			$langArr[$newLang] = (isset($langArr[$newLang])) ?
+				max($langArr[$newLang], $newQual) : $newQual;
+		}
 
-    // Set the most preferred language that we have a translation for.
-    foreach ($acceptedLanguages as $preferredLanguage)
-    {
-        if (in_array($preferredLanguage, $websiteLanguages))
-        {
-            $_SESSION['lang'] = $preferredLanguage;
-            return strtolower($preferredLanguage);
-        }
-    }
+		// sort list based on value
+		// langArr will now be an array like: array('EN' => 1, 'ES' => 0.5)
+		arsort($langArr, SORT_NUMERIC);
+
+		// The languages the client accepts in order of preference.
+		$acceptedLanguages = array_keys($langArr);
+
+		// Set the most preferred language that we have a translation for.
+		foreach ($acceptedLanguages as $preferredLanguage) {
+			if (in_array($preferredLanguage, $websiteLanguages)) {
+				$_SESSION['lang'] = $preferredLanguage;
+				return strtolower($preferredLanguage);
+			}
+		}
+	}
 }
